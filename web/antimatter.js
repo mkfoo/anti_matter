187eccc01e5878644dd9261b619519392b9fbcda
mkfoo <89305338+mkfoo@users.noreply.github.com>
2022-02-23 17:03:54 +0200

    webgl renderer (test)

--- web/antimatter.js
+++ web/antimatter.js
@@ -1,6 +1,8 @@
 class WasmGame {
-    constructor(name) {
+    constructor(name, width, height) {
         this.name = name;
+        this.width = width;
+        this.height = height;
         this.events = [];
     }
 
@@ -71,7 +73,13 @@ class WasmGame {
         this.exports = inst.exports;
         this.update = inst.exports.am_update;
         this.memory = inst.exports.memory.buffer;
-        this.renderer = new CanvasRenderer(this.palette);
+
+        try {
+            this.renderer = new WebGLRenderer(this.palette, this.width, this.height);
+        } catch (err) {
+            console.log(err.message + "Falling back on canvas rendering.");
+            this.renderer = new CanvasRenderer(this.palette, this.width, this.height);
+        }
 
         const pixelData = this.loadPixelData();
         await this.renderer.loadTexture(pixelData);
@@ -94,10 +102,12 @@ class WasmGame {
         });
 
         const nextFrame = (timestamp) => {
+            this.renderer.clear();
+
             if (this.update(timestamp)) {
                 window.requestAnimationFrame(nextFrame);
             } else {
-                this.renderer.quit();
+                this.renderer.clear();
                 this.audio.quit();
             }
         };
@@ -112,36 +122,246 @@ class WasmGame {
         const dataPtr = struct[0];
         const width = struct[1];
         const height = struct[2];
-        const input = new Uint8Array(this.memory, dataPtr, width * height);
-        const buf = new Uint8Array(input.length * 4);
-
-        for (let i = 0; i < input.length; i++) {
-            const j = i * 4;
-            const c = input[i];
-            buf[j + 0] = this.palette[c][0];
-            buf[j + 1] = this.palette[c][1];
-            buf[j + 2] = this.palette[c][2];
-            buf[j + 3] = this.palette[c][3];
-        }
-        
+        const buf = new Uint8Array(this.memory, dataPtr, width * height);
         return { buf, width, height };
     }
 }
 
-class CanvasRenderer {
-    constructor(palette) {
-        const canvas = document.getElementById("canvas"); 
-        this.origWidth = canvas.width;
-        this.origHeight = canvas.height;
-        this.ctx = canvas.getContext("2d");
-        this.ctx.imageSmoothingEnabled = false;
-        this.cssPalette = palette.map(c => { return `rgb(${c[0]}, ${c[1]}, ${c[2]}, ${c[3]})` });
+class WebGLRenderer {
+    constructor(palette, width, height) {
+        const vsSource = `
+            attribute vec4 aDstQuad;
+            varying highp vec2 vSrcXY;
+
+            uniform mat4 uSrcMatrix;
+            uniform mat4 uDstMatrix; 
+
+            void main(void) {
+                vSrcXY = (uSrcMatrix * vec4(aDstQuad.z, aDstQuad.w, 0.0, 1.0)).xy;
+                gl_Position = uDstMatrix * vec4(aDstQuad.x, aDstQuad.y, 0.0, 1.0);
+            }
+        `;
+
+        const fsSource = `
+            varying highp vec2 vSrcXY;
+            uniform sampler2D uSampler;
+
+            void main(void) {
+                gl_FragColor = texture2D(uSampler, vSrcXY);
+            }
+        `;
+       
+        const canvas = document.createElement("canvas");
+        document.body.appendChild(canvas);
+        const gl = canvas.getContext("webgl");
+
+        if (!gl) {
+            throw new Error("Could not create WebGL context.");
+        }
+
+        this.gl = gl;
+        this.palette = palette;
+        this.origWidth = width;
+        this.origHeight = height;
         this.setScaleFactor(2);
+        this.program = this.initProgram(vsSource, fsSource);
+
+        this.attributes = {
+            aDstQuad: gl.getAttribLocation(this.program, "aDstQuad"),
+        };
+
+        this.uniforms = {
+            uSrcMatrix: gl.getUniformLocation(this.program, "uSrcMatrix"),
+            uDstMatrix: gl.getUniformLocation(this.program, "uDstMatrix"),
+            uSampler: gl.getUniformLocation(this.program, "uSampler"),
+        };
+
+        this.buffers = this.initBuffers();
+    }
+
+    initProgram(vsSource, fsSource) {
+        const gl = this.gl;
+        const vs = this.loadShader(gl.VERTEX_SHADER, vsSource);
+        const fs = this.loadShader(gl.FRAGMENT_SHADER, fsSource);
+        const program = gl.createProgram();
+        gl.attachShader(program, vs);
+        gl.attachShader(program, fs);
+        gl.linkProgram(program);
+
+        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            throw new Error(gl.getProgramInfoLog(program));
+        }
+
+        return program;
+    }
+
+    loadShader(type, source) {
+        const gl = this.gl;
+        const shader = gl.createShader(type);
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            throw new Error(gl.getShaderInfoLog(shader));
+        }
+
+        return shader;
+    }
+
+    initBuffers() {
+        const gl = this.gl;
+        const dstQuad = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, dstQuad);
+        gl.bufferData(gl.ARRAY_BUFFER, 16 * 4, gl.DYNAMIC_DRAW);
+        gl.vertexAttribPointer(this.attributes.aDstQuad, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(this.attributes.aDstQuad);
+        return { dstQuad };
+    }
+
+    async loadTexture(pixelData) {
+        const gl = this.gl;
+        const len = pixelData.buf.length;
+        const tw = pixelData.width;
+        const th = pixelData.height;
+        const rgba = new Uint8Array(len * 4);
+
+        for (let i = 0; i < len; i++) {
+            const j = i * 4;
+            const c = pixelData.buf[i];
+            rgba[j + 0] = this.palette[c][0];
+            rgba[j + 1] = this.palette[c][1];
+            rgba[j + 2] = this.palette[c][2];
+            rgba[j + 3] = this.palette[c][3];
+        }
+
+        const texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, tw, th, 0, gl.RGBA, gl.UNSIGNED_BYTE, rgba);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.activeTexture(gl.TEXTURE0);
+        gl.enable(gl.BLEND);
+        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+        this.initMatrices(tw, th);
+        return texture;
+    }
+
+    initMatrices(textureW, textureH) {
+        const gl = this.gl;
+        const sx = 1 / textureW;
+        const sy = 1 / textureH;
+        const dx = -2 * 1 / -this.origWidth;
+        const dy = -2 * 1 / this.origHeight;
+
+        const srcMatrix = [sx,  0,  0,  0,
+                            0, sy,  0,  0,
+                            0,  0,  1,  0,
+                            0,  0,  0,  1];
+
+        const dstMatrix = [dx,  0,  0,  0,
+                            0, dy,  0,  0,
+                            0,  0, -1,  0,
+                           -1,  1,  0,  1];
+
+        gl.useProgram(this.program);
+        gl.uniformMatrix4fv(this.uniforms.uSrcMatrix, false, srcMatrix);
+        gl.uniformMatrix4fv(this.uniforms.uDstMatrix, false, dstMatrix);
+    }
+
+    textureCopy(sx, sy, sw, sh, dx, dy) {
+        const dw = sw;
+        const dh = sh;
+        const gl = this.gl;
+
+        const verts = [
+            dx + dw, dy + dh, sx + sw, sy + sh,
+            dx +  0, dy + dh, sx +  0, sy + sh,
+            dx + dw, dy +  0, sx + sw, sy +  0,
+            dx +  0, dy +  0, sx +  0, sy +  0,
+        ];
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers.dstQuad);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(verts));
+        gl.useProgram(this.program);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    }
+
+    fillRect(x, y, w, h, c) {
+    }
+
+    drawLine(x1, y1, x2, y2, c) {
+    }
+
+    toggleScaleFactor() {
+        switch (this.scaleFactor) {
+            case 1:
+                this.setScaleFactor(2);
+                break;
+            case 2:
+                this.setScaleFactor(4);
+                break;
+            default:
+                this.setScaleFactor(1);
+        }
+    }
+
+    setScaleFactor(sf) {
+        const gl = this.gl;
+        gl.canvas.width = this.origWidth * sf;
+        gl.canvas.height = this.origHeight * sf;
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        this.scaleFactor = sf;
+    }
+
+    clear() {
+        const gl = this.gl;
+        gl.clearColor(0.0, 0.0, 0.0, 1.0); 
+        gl.clearDepth(1.0); 
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    }
+}
+
+class CanvasRenderer {
+    constructor(palette, width, height) {
+        const oldCanvas = document.querySelector("canvas");
+
+        if (oldCanvas) {
+            document.body.removeChild(oldCanvas);
+        }
+
+        const canvas = document.createElement("canvas");
+        document.body.appendChild(canvas);
+        this.ctx = canvas.getContext("2d");
+
+        if (!this.ctx) {
+            document.body.removeChild(canvas);
+            const p = document.createElement("p");
+            p.textContent = "Sorry, your browser does not support canvas rendering.";
+            document.body.appendChild(p);
+            throw new Error("Could not create 2D context.");
+        }
+
+        this.origWidth = width;
+        this.origHeight = height;
+        this.setScaleFactor(2);
+        this.cssPalette = palette.map(c => { return `rgb(${c[0]}, ${c[1]}, ${c[2]}, ${c[3]})` });
+        this.palette = palette;
     }
 
     async loadTexture(pixelData) {
         const img = this.ctx.createImageData(pixelData.width, pixelData.height);
-        img.data.set(pixelData.buf);
+
+        for (let i = 0; i < pixelData.buf.length; i++) {
+            const j = i * 4;
+            const c = pixelData.buf[i];
+            img.data[j + 0] = this.palette[c][0];
+            img.data[j + 1] = this.palette[c][1];
+            img.data[j + 2] = this.palette[c][2];
+            img.data[j + 3] = this.palette[c][3];
+        }
+
         this.texture = await createImageBitmap(img);
     }
 
@@ -184,8 +404,8 @@ class CanvasRenderer {
         this.scaleFactor = sf;
     }
 
-    quit() {
-        this.ctx.clearRect(0, 0, 256, 192);
+    clear() {
+        this.ctx.clearRect(0, 0, this.origWidth, this.origHeight);
     }
 }
 
@@ -218,4 +438,4 @@ class AudioSubsystem {
     }
 }
 
-new WasmGame("antimatter").main();
+new WasmGame("antimatter", 256, 192).main();
